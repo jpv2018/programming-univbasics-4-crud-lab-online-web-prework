d68d1fead126b49c07f562dfee282cc78349e466
Jason Viola <jason.viola@mavs.uta.edu>
2019-06-28 22:37:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,6 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   index_number = "#{array[2]}"
 end
 
