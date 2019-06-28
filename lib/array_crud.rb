314bc7ce81f69c026a3375dd47cda38938104a58
Jason Viola <jason.viola@mavs.uta.edu>
2019-06-28 22:38:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,6 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   p array[0]
 end
 
