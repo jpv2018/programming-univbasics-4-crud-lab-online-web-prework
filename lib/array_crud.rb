cd47f4b707fb2af2a4b41e45a5b03bf5d19fa64b
Jason Viola <jason.viola@mavs.uta.edu>
2019-06-28 22:38:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,6 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   p array[-1]
 end
 
